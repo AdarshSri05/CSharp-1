6a8f974771c122ffc70483e44ecb09267fc755ee
adarsh4505 <adarsh9628281469@gmail.com>
2023-07-06 20:35:13 +0530

    Additional programs

---
+++ Basics/Sum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class Sum
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Enter the num1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum = {0}", a + b);
+            Console.ReadLine();
+
+
+        }
+    }
+}
