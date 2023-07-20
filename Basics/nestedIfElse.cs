29685a4fe0e6df3bcdd3b947d658bb36cc8a609e
adarsh4505 <adarsh9628281469@gmail.com>
2023-07-20 09:17:20 +0530

    loops2

--- Basics/nestedIfElse.cs
+++ Basics/nestedIfElse.cs
@@ -8,5 +8,26 @@ namespace Basics
 {
     internal class nestedIfElse
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 50)
+            {
+                if (number % 2 == 0) 
+                {
+                    Console.WriteLine(number * 2);
+                }
+                else
+                {
+                    Console.WriteLine(number * 3);
+                }
+            }
+            else
+            {
+                Console.WriteLine(number * 5);
+            }
+            Console.ReadLine();
+        }
     }
 }
